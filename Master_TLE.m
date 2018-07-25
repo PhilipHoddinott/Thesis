32c49f15aa510801991ffd72b18e56e9428de129
PhilipHoddinott <philiphoddinott@gmail.com>
2018-07-25 15:32:52 -0400

    Added in Orbit visulizationand cites
    Need to update Wiki, and also start working on calculations from one orbit to another

--- Master_TLE.m
+++ Master_TLE.m
@@ -4,43 +4,41 @@
 close all; clear all; clc; % clear workspace
 %% get data
 VarStore % run var store for stored variables, ugly but it works
-%% func 
+%% func get_SATCAT
+% Function to get a .mat file from the SATCAT
+%%%%%%%%%%%%%%%%%%%%%%%%% Uncomment to run
 %get_SATCAT % get SATCAT, comment out if already run
-%clear all; % clear those variables, comment of already run
-VarStore % run var store for stored variables, ugly but it works
 
 strNamMat = ['SATCAT_',num2str(launchYear),'.mat'];
 load(strNamMat); % load mat of SATCAT
 
 relDeb=str2num(char(all_TLE(2:decayEnd,2))); % get NORAD CAT ID
 
-%% func
+%% func get_Multiple_TLE_from_Id
+% Function to get tle txt files from the norat_cat_ids in the SATCAT
 VarStore % run var store for stored variables, ugly but it works
+%%%%%%%%%%%%%%%%%%%%%%%%% Uncomment to run
 %get_Multiple_TLE_from_Id % returns 'outStr' String of TLE
-%% func % THIS FUNC NO LONGER USED
-%save_TLE % Saves TLEs as txt file
-% this is probally un nesssisary, as I could just combind this to 
-%file = 'tle_1_1_500.txt';
-%file =['tle_1_1_',num2str(tle_inc),'.txt'];
-%file = [tle_folder,'/',file];
+
+%% Func readTLE_txt
+% function to parse the txt files into a usable TLE, stored in a matrix
 fprintf('readTLE_txt\n');
 readTLE_txt
 
-%check_TLE
+%% Func check_TLE_edit_TLE
+% function to neatly sort TLEs, remove duplicates, and list TLEs that were
+% not given
 fprintf('check_TLE_Edit_TLE\n');
 check_TLE_Edit_TLE
 
-fprintf('pickUP_TLE\n');
-pickUP_TLE
+strNam = ['TLE_',num2str(launchYear),'.mat']; % save the TLE as a .mat
+save(strNam,'tle_final');
 
-fprintf('readTLE_PU_txt\n');
-readTLE_PU_txt
 
-fprintf('append_TLE\n');
-append_TLE
-%% TO DO
-% Make the funcitons more atnomus, call loops inside the master program
-% like the get_TLE stuff
+close all; clear all; % clear out everything
+VarStore % run var store for stored variables, ugly but it works
+strNam = ['TLE_',num2str(launchYear),'.mat']; % get strNam
 
+load(strNam, 'tle_final')
 
 
--- check_TLE_Edit_TLE.m
+++ check_TLE_Edit_TLE.m
@@ -2,15 +2,15 @@
 % This function checks the TLEs I have against the TLEs I wanted to have.
 % It is part of my attempt to dealwith the repeating TLEs, I am dealing
 % with this by varying the number of TLEs I grab
+
 close all;
 tle_stor_current=[];
-end_c_T=decayEnd-1; % 4800
-
+end_c_T=decayEnd; % 4800
 for ik=1:length(tleA)-1
     tle_stor_temp=tle_stor_Main{ik};
     tle_stor_current=[tle_stor_current;tle_stor_temp(2:tle_inc+1,:)];
 end
-tleU=unique(tle_stor_current(:,:),'rows');
+% tleU=unique(tle_stor_current(:,:),'rows');
 [C,ia,ic]=unique(tle_stor_current(:,1),'rows');
 tleUP = tle_stor_current(ia,:);
 
@@ -58,75 +58,14 @@ for i=1:length(relDebCheck)-1
     tleCompCheck(i,:)=[relDebCheck(i),tleUP(i,:)];
 end
 
-        
-%{
-tleComp=[];
-for i=1:length(relDeb)
-    tleComp(i,:)=[relDeb(i),tleU(i,:)];
-end
-tleU1=tleU;
-for i=3:length(tleU)
-    curr_cat=tleU(i,1);
-    yn=0;
-    for j=1:i-1
-        if curr_cat==tleU(j,1)
-            yn=yn+1;
-            locP=j;
-        end
-    end
-    if yn~=0
-        tleU1(i,:)=[];
-        %tleU1=[tleU1(1:i-1,:);tleU1(i+1:end,:)];
-        fprintf('removed dup ID %d from line %d, also at %d, lenght now %d\n',curr_cat,i,locP,length(tleU1));
+tle_final=tleCompCheck;
+float_length=length(tle_final);
+for i=1:float_length;
+    if tle_final(i,1)==tle_final(i,3)
+        tle_final(i,:)=[];
+        float_length=length(tle_final);
     end
 end
-
-for i=1:length(tleU1)
-    tleU1Comp(i,:)=[relDeb(i),tleU1(i,:)];
-end
-
-%}        
-            
-    
+tle_final(:,1)=[];
 
     
-
-%{
-for ik=1:3%12 % till i fix this
-    tle_stor_current=tle_stor_Main{ik};
-    
-    lne = 2:decayEnd;
-    f1=(2*(ik-1))+1; % figure names
-    f2=(2*(ik-1))+2; % figure names
-    figure(f1)
-    hold on
-    plot(lne(1:tle_inc),relDeb(tleA(ik):tleA(ik+1)-1),'-x')
-    plot(lne(1:tle_inc), tle_stor_current(2:tle_inc+1,1),'-o')
-    grid on
-
-    stDiff = tleA(ik+1)-10;
-    diff(:)=tle_stor_current(1:tle_inc,1)-relDeb((tle_inc*(ik-1))+1:tle_inc*(ik));
-    %for i=1:tle_inc-1
-    %    diff(i)=tle_stor_current(i+1,1)-relDeb(i);
-    %end
-    
-    figure(f2)
-    plot(lne(tleA(ik):tleA(ik+1)-1),diff)
-    grid on
-    
-    for i=stDiff:tleA(ik+1)
-        fprintf('%d, ',relDeb(i));
-    end
-    fprintf('\n');
-    for i=stDiff+1:tle_inc+1
-        fprintf('%d, ',tle_stor_current(i,1));
-    end
-    fprintf('\n');
-    for i=1:tle_inc
-        fprintf('%d, ',diff(i));
-    end
-    fprintf('\n');
-    %relDeb(492:500)
-    %tle_stor_1_500(493:501,1)
-end
-%}
